8c40bf84be20f1d3a1ba63453ffafd58799cfee2
Xiphereal <lozanojalex@gmail.com>
2024-09-29 18:37:36 +0200

    refactor: remove nodes path duplication

--- scenes/inventory.gd
+++ scenes/inventory.gd
@@ -1,11 +1,13 @@
 extends Control
 
 var current_page := 1
+@onready var previous_button := $PanelContainer/VBoxContainer/HBoxContainer/Previous
+@onready var next_button := $PanelContainer/VBoxContainer/HBoxContainer/Next
 
 func _ready() -> void:
-	$PanelContainer/VBoxContainer/HBoxContainer/Previous.disabled = true
-	$PanelContainer/VBoxContainer/HBoxContainer/Next.disabled = false
+	previous_button.disabled = true
+	next_button.disabled = false
 
 func _on_next_pressed() -> void:
 	current_page += 1
-	$PanelContainer/VBoxContainer/HBoxContainer/Next.disabled = true
+	next_button.disabled = true
