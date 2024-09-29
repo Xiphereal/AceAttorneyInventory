62c9f5a2f5639198e3550d039fc543b4d41e6f28
Sarius97 <rgcabero197@gmail.com>
2024-09-29 19:00:44 +0200

    refactor: update page number on single place

--- scenes/inventory.gd
+++ scenes/inventory.gd
@@ -8,17 +8,19 @@ var current_page := 1
 func _ready() -> void:
 	previous_button.disabled = true
 	next_button.disabled = false
-
+	
+func _process(delta):
+	page_number.text = str(current_page) + "/2"
+	
 func _on_previous_pressed() -> void:
 	current_page -= 1
 	
-	page_number.text = str(current_page) + "/2"
+	
 	previous_button.disabled = true
 	next_button.disabled = false
 
 func _on_next_pressed() -> void:
 	current_page += 1
 	
-	page_number.text = str(current_page) + "/2"
 	previous_button.disabled = false
 	next_button.disabled = true
